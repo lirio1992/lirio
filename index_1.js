e5fc11b6c1860249fac2db93b6d2039b0c5a9b20
lirio1992 <125261571+lirio1992@users.noreply.github.com>
2023-02-13 21:14:16 +0100

    Create index_1.js

---
+++ index_1.js
@@ -0,0 +1,38 @@
+function abc (){
+let myRoom = "roomitem_3ekbkfAqJ20k",
+myFirstRoom = "roomcategory_n";
+function dibraStar(){ 
+if ( document.getElementById(`${myFirstRoom }`) ) 
+{document.getElementById(`${myFirstRoom }`).click();}
+if ( document.getElementById(`${myRoom}`) ) 
+{document.getElementById(`${myRoom}`).click();}
+flood()
+if(document.getElementsByClassName("GameItemStart")[0])
+{document.getElementsByClassName("GameItemStart")[0].click();}
+if (document.getElementsByClassName("InvitationGame StartGame")[0])
+{ document.getElementsByClassName("InvitationGame StartGame")[0].click();}
+if (document.getElementsByClassName("SButton")[0]) { 
+setTimeout(ke, 5000);	
+ }
+setTimeout(dibraStar, 5000);
+}
+function flood(){ 
+if (document.getElementsByClassName("SButton")[0]) { 
+if (document.getElementById("RoomChatInput")) { 
+document.getElementById("RoomChatInput").value = "O-)"
+document.getElementById("RoomChatButton").click();
+ }
+}
+ setTimeout(flood, 1000);
+}
+function ke(){ 
+if (document.getElementsByClassName("SButton")[0]) { 
+document.getElementsByClassName("SButton")[0].click();
+}
+}
+dibraStar()
+
+}
+abc ();
+
+
